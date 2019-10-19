a98a8416416e6cfd37f18a040fa39bc8c615568f
El allaoui El mehdi <elmehdi.elallaoui@edu.uiz.ac.ma>
2019-10-19 23:32:48 +0100

    changes

--- src/Java/Elallaoui/ProjetStation/GasTruck.java
+++ src/Java/Elallaoui/ProjetStation/GasTruck.java
@@ -1,42 +1,19 @@
 package Java.Elallaoui.ProjetStation;
 
-import com.sun.org.apache.xerces.internal.parsers.CachingParserPool;
-
 import javax.imageio.ImageIO;
-import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 
-import static java.lang.Thread.sleep;
+public class GasTruck extends Car {
 
-public class Car {
-    protected static final int D_W = 800;
-    protected static final int D_H = 400;
-    protected static final int INCREMENT = 5;
-    int x, y;
-    int rand;
-    GasContainer gasContainer ;
 
-    protected   Image car;
 
-    public Car(int x, int y,GasContainer gasContainer) {
-        this.x = x;
-        this.y = y;
-        this.gasContainer = gasContainer;
-
-         rand = (int) (Math.random()*4);
-        try {
-            car = ImageIO.read(new File("new_car_image_"+rand+".png" ));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-    public void drawCar(Graphics g) {
-        g.drawImage(car,x,y,null);
+    public GasTruck(int x, int y, GasContainer gasContainer) {
+        super(x, y, gasContainer);
     }
 
+    @Override
     public void move() throws IOException {
-
         if(x == D_W / 2) {
             try {
                 gasContainer.fillCar((int) (Math.random()*50));
@@ -60,8 +37,5 @@ public class Car {
             x -= INCREMENT;
 
         }
-
-
-
     }
 }
